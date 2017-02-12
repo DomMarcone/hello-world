2f1097c59019280b687104f13ac5c0818f78a6be
DomMarcone <dammarcone@yahoo.com>
2017-02-12 08:50:41 -0500

    Added an index.php and removed index.htm

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+
+<style>
+
+.dom-catagory
+{
+	
+	
+}
+
+</style>
+</head>
+<body>
+	<div class="dom-catagory" id="dom-bio"></div>
+	<div class="dom-catagory" id="dom-skills"></div>
+
+</body>
+</html>
